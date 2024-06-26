13adbcb90441b849e0d6133a6d501a1bd60203f5
Fareda Elsayed <faredaelsayed0@gmail.com>
2024-06-26 14:17:07 +0300

    #	modified:   src/pages/ProtectedRoute.jsx

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -3,9 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../contexts/Auth";
 
 const ProtectedRoute = () => {
-  const isLoggedIn = localStorage.getItem("isLoggedIn");
   const { isAuthenticated } = useAuth();
-  console.log(isLoggedIn);
 
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
